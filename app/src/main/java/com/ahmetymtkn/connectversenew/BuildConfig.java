f8fa8569baa725ba23cd3926ac682659410e70b7
Ahmet Yumutkan <113467735+ahmetymtkn@users.noreply.github.com>
2024-10-09 19:48:32 +0300

    Update BuildConfig.java (deleted api key)

--- app/src/main/java/com/ahmetymtkn/connectversenew/BuildConfig.java
+++ app/src/main/java/com/ahmetymtkn/connectversenew/BuildConfig.java
@@ -1,7 +1,7 @@
 package com.ahmetymtkn.connectversenew;
 
 public class BuildConfig {
-    public static String apiKey = "AIzaSyCB83m3UcnZtqZTbY4a7GEjp5iAlBww0DI";
+    public static String apiKey = "Gemini_api_key";
 
 
 }
